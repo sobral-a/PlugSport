ad09f177fa9271236e1ef7d2f836196525e15bd4
Alexandre <alexandre@realytics.io>
2017-06-19 15:08:30 +0200

    Create users database and add register mechanism and page

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -48,7 +48,9 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|string|max:255',
+            'name' => 'required|string|min:2|max:255',
+            'first_name' => 'required|string|min:2|max:255',
+            'profil' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
@@ -64,6 +66,8 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'first_name' => $data['first_name'],
+            'profil' => $data['profil'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
