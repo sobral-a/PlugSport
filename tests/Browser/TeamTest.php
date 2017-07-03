d9659d7fc60df9c3f35e016e2e29d58284648b80
Khalis <khalis.chalabi@epita.fr>
2017-07-04 00:38:33 +0200

    TeamTest: add test create a team

--- tests/Browser/TeamTest.php
+++ tests/Browser/TeamTest.php
@@ -35,12 +35,24 @@ class TeamTest extends DuskTestCase
     public function testCreateTeamFailed()
     {
         $this->browse(function ($browser) {
-            $browser->visit('/login')
-                ->type('email', "khalis@hotmail.fr")
-                ->type('password',  "test75")
-                ->press('Se connecter')
-                ->clickLink('Equipes')
-                ->select('size')
+            $browser->clickLink('Equipes')
+                ->select('sport')
+                ->assertDontSee('MTI Club');
+        });
+    }   
+    
+    /**
+     * @group TeamTest
+     *
+     * @return void
+     */
+    public function testCreateTeamSuccessed()
+    {
+        $this->browse(function ($browser) {
+            $browser->clickLink('Equipes')
+                ->type('name', 'MTI Club')
+                ->select('sport')
+                ->press('Créer')
                 ->assertSee('MTI Club');
         });
     }   
